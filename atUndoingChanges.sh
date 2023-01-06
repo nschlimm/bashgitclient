65d8481d6179c09c50151dd03b5ef600faa3e249
Niklas Schlimm <nschlimm@timocom.com>
2023-01-06 15:54:25 +0100

    New commands

--- atUndoingChanges.sh
+++ atUndoingChanges.sh
@@ -18,12 +18,22 @@ function resetToCommit () {
    git reset $cname
 }
 
+function resetToCommitHard () {
+   echo "Last 15 commits"
+   git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -n 100
+   echo "Enter commit name"
+   read cname
+   [ "${cname}" = "" ] && waitonexit && return 
+   git reset --hard $cname 
+}
+
 while ${continuemenu:=true}; do
 clear
 menuInit "Undoing changes"
 submenuHead "Undoing changes"
 menuPunkt a "Revert commit - (keep commit history)" revertToCommit
-menuPunkt b "Reset commit - (delete some commits)" resetToCommit
+menuPunkt b "(Soft) Reset commit - (delete some commits)" resetToCommit
+menuPunkt c "(Hard) Reset commit - (delete some commits)" resetToCommitHard
 
 choice
 done
